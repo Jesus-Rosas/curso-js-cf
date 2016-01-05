310c3dd4fe510af46fd19943910be17d671af693
Jesus Rosas <infinitdeveloperofficial@gmail.com>
2016-01-05 04:30:07 -0600

    Parte 2 del juego

--- MVC/ping-pong/main.js
+++ MVC/ping-pong/main.js
@@ -15,12 +15,35 @@
 		this.ball = null;
 	}
 
-	Board.prototype = {
+	self.Board.prototype = {
 		get elements(){
 			var elements = this.bars;
-			elements.push(ball);
+			elements.push(this.ball);
 			return elements;
 		}		
+	}
+})();
+
+//Funcion barras
+
+(function(){
+	self.Bar = function(x, y, width, height, board){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.board = board;
+		this.board.bars.push(this);
+		this.kind = "rectangle";
+		console.log("Hola Mundo");
+	}
+	self.Bar.prototype = {
+		down: function(){
+
+		},
+		up: function(){
+
+		}
 	}
 })();
 
@@ -33,12 +56,34 @@
 		this.board = board;
 		this.ctx = canvas.getContext("2d");
 	}
+	self.BoardView.prototype = {
+		draw: function(){
+			for(var i = this.board.elements.length - 1 ; i >= 0 ; i--){
+				var el = this.board.elements[i];
+
+				draw(this.ctx, el);
+			}
+		}
+	}
+	function draw(ctx, element){
+		console.log(element);
+		if(element !== null && element.hasOwnProperty("kind")){
+			switch(element.kind){
+				case "rectangle":
+					ctx.fillRect(element.x, element.y, element.width, element.height);
+				break;
+			}
+		}
+	}
 })();
 
 window.addEventListener("load", main);
 function main(){
 	var board = new Board(800, 400);
+	var bar = new Bar(20, 100, 40, 100, board)
+	var bar = new Bar(700, 100, 40, 100, board)
 	var canvas = document.getElementById("canvas");
-
 	var board_view = new BoardView(canvas, board);
+	board_view.draw();
+	console.log(board);
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Curso de JavaScript by Codigo Facilito
 
-Ejercicos y Prácticas del Curso
+Ejercicos y Prácticas del Curso de JavaScript 
+
